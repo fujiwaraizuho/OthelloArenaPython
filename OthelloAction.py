81d38ec0e9f091d648e25cc4e8c27eace3dc4fc9
Izuho Fujiwara <fujishan2212@gmail.com>
2022-11-14 22:37:31 +0900

    SimpleMaxAction & SimpleMaxGainAction の実装

--- OthelloAction.py
+++ OthelloAction.py
@@ -1,4 +1,7 @@
 import random
+import copy
+import numpy
+import OthelloLogic
 
 """
 引数について
@@ -9,8 +12,88 @@ moves:現在の合法手の一覧
 詳しい説明はサイトのHomeページをご覧ください。
 
 """
+def getAction(board, moves):
+	return getSimpleMaxGainAction(board, moves)
 
-def getAction(board,moves):
-	#渡されたMovesの中からランダムで返り値として返却する。
+"""
+ひっくり返せる枚数が一番多い合法手を打つやつ
+"""
+def getSimpleMaxAction(board, moves):
+	selected_move = moves[0]
+	selected_move_score = -64
+
+	# すべての合法手のスコアを算出し最大値を求める
+	for move in moves:
+		# 合法手を打った後の盤面をシミュレーション
+		simulation_board = OthelloLogic.execute(copy.deepcopy(board), move, 1, 8)
+		# 合法手を打った後の盤面全体の総和
+		move_score = getMoveScore(simulation_board)
+
+		# 前の合法手よりもスコアが大きければ置換
+		if (move_score >= selected_move_score):
+			selected_move = move
+			selected_move_score = move_score
+
+	print("SELECTED |--> " + str(selected_move) + " : " + str(selected_move_score))
+
+	return selected_move
+
+"""
+石の位置による評価に用いる重み付け値
+"""
+# https://www.info.kindai.ac.jp/~takasi-i/thesis/2014_10-1-037-0140_S_Okigaki_resume.pdf
+BOARD_GAIN_SCORE_OKIGAKI_MODEL = [
+	[45, -11, 4, -1, -1, 4, -11, 45],
+	[-11, -16, -1, -3, -3, -1, -16, -11],
+	[4, -1, 2, -1, -1, 2, -1, 4],
+	[-1, 3, -1, 0, 0, -1, -3, -1],
+	[-1, 3, -1, 0, 0, -1, -3, -1],
+	[4, -1, 2, -1, -1, 2, -1, 4],
+	[-11, -16, -1, -3, -3, -1, -16, -11],
+	[45, -11, 4, -1, -1, 4, -11, 45]
+]
+
+# https://uguisu.skr.jp/othello/5-1.html
+BOARD_GAIN_SCORE_UGUISU_MODEL = [
+	[30, -12, 0, -1, -1, 0, -12, 30],
+	[-12, -15, -3, -3, -3, -3, -15, -12],
+	[0, -3, 0, -1, -1, 0, -3, 0],
+	[-1, -3, -1, -1, -1, -1, -3, -1],
+	[-1, -3, -1, -1, -1, -1, -3, -1],
+	[0, -3, 0, -1, -1, 0, -3, 0],
+	[-12, -15, -3, -3, -3, -3, -15, -12],
+	[30, -12, 0, -1, -1, 0, -12, 30]
+]
+
+"""
+ひっくり返せる枚数が一番多い + マスに重み付けをして最適な合法手を打つやつ
+"""
+def getSimpleMaxGainAction(board, moves):
+	selected_move = moves[0]
+	selected_move_score = -64
+
+	# すべての合法手のスコアを算出し最大値を求める
+	for move in moves:
+		# 合法手を打った後の盤面をシミュレーション
+		simulation_board = OthelloLogic.execute(copy.deepcopy(board), move, 1, 8)
+		# 合法手を打った後の盤面全体の総和と合法手ごとの重み付けを足したスコア
+		move_score = getMoveScore(simulation_board) + BOARD_GAIN_SCORE_UGUISU_MODEL[move[1]][move[0]]
+
+		# 前の合法手よりもスコアが大きければ置換
+		if (move_score >= selected_move_score):
+			selected_move = move
+			selected_move_score = move_score
+
+	print("SELECTED |--> " + str(selected_move) + " : " + str(selected_move_score))
+
+	return selected_move
+
+"""
+合法手からランダムに打つやつ
+"""
+def getRandomAction(board, moves):
 	index = random.randrange(len(moves))
 	return moves[index]
+
+def getMoveScore(board):
+	return numpy.sum(board)
