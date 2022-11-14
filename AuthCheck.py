b58a02d57d6f4c9f582b0bf866fc419a9fd8c91f
Izuho Fujiwara <fujishan2212@gmail.com>
2022-11-14 22:34:21 +0900

    fix indent & add requirements.txt

--- AuthCheck.py
+++ AuthCheck.py
@@ -1,5 +1,4 @@
 import requests
-import json
 import os
 
 def auth_check(base_url):
--- Play.py
+++ Play.py
@@ -9,24 +9,23 @@ headers = AuthCheck.auth_check(base_url)
 
 r = requests.post(base_url + "where", headers=headers)
 
-if(r.status_code == 422):
-	message = r.json()
-	print(message['message'])
-	exit()
-if(r.status_code != requests.codes.ok):
-	print('エラーが発生しました。')
-	exit()
+if (r.status_code == 422):
+    message = r.json()
+    print(message['message'])
+    exit()
+if (r.status_code != requests.codes.ok):
+    print('エラーが発生しました。')
+    exit()
 
 data = r.json()
-roomid = data['id']
+room_id = data['id']
 player = data['player']
 board = []
 payload = {}
 
-
-if(player == 1):
-    payload = {'player':player}
-    r = requests.post(base_url + "wait_for_player/" + roomid, data=payload,headers=headers)
+if (player == 1):
+    payload = {'player': player}
+    r = requests.post(base_url + "wait_for_player/" + room_id, data=payload, headers=headers)
     data = r.json()
     OthelloLogic.printBoard(json.loads(data['board']))
     board = json.loads(data['board'])
@@ -40,35 +39,33 @@ else:
             board[x][y] = rev_board[x][y] * -1
     moves = json.loads(data['moves'])
 
+action = json.dumps(OthelloAction.getAction(board, moves))
+payload = {'action': action, 'player': player}
 
-action = json.dumps(OthelloAction.getAction(board,moves))
-payload = {'action' : action,'player':player}
-
-while(True):
+while (True):
     print("打った場所は" + action)
-    r = requests.post(base_url + "room/" + roomid, data=payload,headers=headers)
-    if(r.status_code == 401):
+    r = requests.post(base_url + "room/" + room_id, data=payload, headers=headers)
+    if (r.status_code == 401):
         print('認証エラーが起きました。')
         exit()
-    if(r.status_code != requests.codes.ok):
-	    print('エラーが発生しました。')
-	    exit()
+    if (r.status_code != requests.codes.ok):
+        print('エラーが発生しました。')
+        exit()
     data = r.json()
-    if(data['finish_flag']):
-    	print('処理が終了しました。')
-    	exit()
+    if (data['finish_flag']):
+        print('処理が終了しました。')
+        exit()
 
     OthelloLogic.printBoard(json.loads(data['board']))
-    if(player == -1):
-    	rev_board = json.loads(data['board'])
-    	board = json.loads(data['board'])
-    	for x in range(len(rev_board)):
+    if (player == -1):
+        rev_board = json.loads(data['board'])
+        board = json.loads(data['board'])
+        for x in range(len(rev_board)):
             for y in range(len(rev_board)):
                 board[x][y] = rev_board[x][y] * -1
     else:
-    	board = json.loads(data['board'])
+        board = json.loads(data['board'])
 
     moves = json.loads(data['moves'])
-    action = json.dumps(OthelloAction.getAction(board,moves))
-    payload = {'action' : action,'player':player}
-
+    action = json.dumps(OthelloAction.getAction(board, moves))
+    payload = {'action': action, 'player': player}
